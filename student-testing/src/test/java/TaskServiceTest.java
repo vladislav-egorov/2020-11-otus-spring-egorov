b63755cae5fba619019efd5743ff19ea24360a7c
Vladislav <egorov.vladislav@outlook.com>
2020-12-06 15:15:30 +0300

    refactor unit tests

--- student-testing/src/test/java/TaskServiceTest.java
+++ student-testing/src/test/java/TaskServiceTest.java
@@ -1,44 +1,47 @@
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.students.testing.dao.TaskDao;
+import ru.otus.students.testing.domain.Answer;
 import ru.otus.students.testing.domain.Task;
 import ru.otus.students.testing.service.TaskService;
+import ru.otus.students.testing.service.TaskServiceImpl;
 
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class TaskServiceTest {
-    private ClassPathXmlApplicationContext context;
-    private TaskService taskService;
 
-    @Before
-    public void before() {
-        context = new ClassPathXmlApplicationContext("/spring-context.xml");
-        taskService = context.getBean(TaskService.class);
+    @Mock
+    TaskDao taskDao;
+    TaskService taskService;
+
+    @BeforeEach
+    void init() {
+        taskService = new TaskServiceImpl(taskDao);
+        List<Answer> answers = List.of(
+                new Answer("answer1", true),
+                new Answer("answer2", false)
+        );
+        Task testTask = new Task("First task", answers);
+        when(taskDao.findAll()).thenReturn(List.of(testTask));
     }
 
     @Test
-    public void allCsvRecordsIsNotNull() {
-        List<Task> allTasks = taskService.findAllTasks();
-        assertNotNull(allTasks);
+    void tasksAreNotNullAndNotEmpty() {
+        assertThat(taskService.findAllTasks()).isNotNull().isNotEmpty();
     }
 
-    @Test
-    public void firstQuestionIs() {
-        String knownQuestion = "Which one number is simple?";
-        List<Task> allTasks = taskService.findAllTasks();
-        Task task = allTasks.get(0);
-        String question = task.getQuestion();
-        assertEquals(knownQuestion, question);
-    }
 
     @Test
-    public void secondQuestionIsNot() {
-        String knownQuestion = "Which one number is simple?";
-        List<Task> allTasks = taskService.findAllTasks();
-        Task task = allTasks.get(1);
-        String question = task.getQuestion();
-        assertNotEquals(knownQuestion, question);
+    void firstTaskRightAnswerIs() {
+        Task task = taskService.findAllTasks().get(0);
+        Answer answer = task.getAnswers().get(0);
+        assertThat(answer.getIsItRight()).isTrue();
     }
 }
