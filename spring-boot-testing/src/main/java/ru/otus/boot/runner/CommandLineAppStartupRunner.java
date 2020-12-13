6c999dc4d9997816e4329b8cabf6a296fffed5d4
Vladislav <egorov.vladislav@outlook.com>
2020-12-13 16:50:10 +0300

    * add tests from old module

--- spring-boot-testing/src/main/java/ru/otus/boot/runner/CommandLineAppStartupRunner.java
+++ spring-boot-testing/src/main/java/ru/otus/boot/runner/CommandLineAppStartupRunner.java
@@ -2,10 +2,12 @@ package ru.otus.boot.runner;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import ru.otus.boot.service.TestingService;
 
 @Component
+@Profile("!test")
 @RequiredArgsConstructor
 public class CommandLineAppStartupRunner implements CommandLineRunner {
 
